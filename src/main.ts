f3c0d25dfd9113076a84b1f18720e49656045c15
juanmaPuhl <puhljm@gmail.com>
2022-09-28 11:59:32 -0300

    Changing env name

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(env.port || 3000);
+  await app.listen(env.PORT || 3000);
 }
 
 bootstrap();
